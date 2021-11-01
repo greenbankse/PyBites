a081ba509ee4a4ac31bc48bab8a4599d9cc9d7cd
greenbankse <samuel.greenbank.battery@gmail.com>
2021-11-01 17:51:43 +0000

    bite 29

--- main/29/wrong_char.py
+++ main/29/wrong_char.py
@@ -1,2 +1,17 @@
 def get_index_different_char(chars):
-    pass
+    loc = [indx for indx, s in enumerate(chars) if not str(s).isalnum()]
+    # if len(loc) == 1:
+    #     return loc[0]
+    # else:
+    #     return [indx for indx, s in enumerate(chars) if str(s).isalnum()][0]
+    return (
+        loc[0] if len(loc) == 1 else [indx for indx, s in enumerate(chars) if str(s).isalnum()][0]
+    )
+
+
+# s = ["d", "g", "3", ".", "g"]
+# print(s)
+# print([indx for indx, s in enumerate(s) if not s.isalnum()][0])
+
+s = ["A", "f", ".", "Q", 2]
+print(get_index_different_char(s))
